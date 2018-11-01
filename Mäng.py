40cfdd0d2cee2fc37f6070daea9bb6dd4d20eb1f
Laima Anna <laima.anna.d@gmail.com>
2018-11-01 17:21:09 +0200

    Things together and time

--- Mäng.py
+++ Mäng.py
@@ -1,7 +1,10 @@
 import pygame
+import os
+import time
 from random import randrange
 from random import randint
 
+os.environ['SDL_VIDEO_CENTERED']='1'
 pygame.init()
 
 white = (255,255,255)
@@ -14,13 +17,15 @@ display_height = 600
 gameDisplay = pygame.display.set_mode((display_width,display_height))
 pygame.display.set_caption('Mäng')
 
+
 clock = pygame.time.Clock()
+
 #update specific area, if no parameters then all
 #pygame.display.update()
 #update all display at once
 #pygame.display.flip()
 
-
+flag=False
    
 
 def message(msg,color):
@@ -42,6 +47,9 @@ def gameLoop():
     block = randint(10,100)
     block_x=randrange(0,display_width-block)
     i=0
+    start_time=time.time()
+    
+    blocks=[]
     
     while not gameExit:
         while gameOver:
@@ -78,11 +86,10 @@ def gameLoop():
 
         lead_x += lead_x_change
  
-        #if lead_x >= display_width-size or lead_x < 0+size:
-        #    lead_x_change=0
             
-            
+        #random falling   
         if block_y<display_height:
+            #block speed
             block_y+=10
         else:
             block_y=0-block
@@ -105,14 +112,22 @@ def gameLoop():
         
         pygame.display.update()
         
+        #collision
         if lead_x>block_x and lead_x<block_x+block or lead_x+size>block_x and lead_x+size<block_x+block:
             if lead_y>block_y and lead_y<block_y+block or lead_y+size>block_y and lead_y+size<block_y+block:
                 print(i)
                 i+=1
                 #gameOver=True
-                
+        
+
+        #moment after 5 sec
+        if time.time() -start_time > 2:
+            print('aaa')
+            start_time=time.time()
+            #blocks.append()
             
-        clock.tick(15)
+            
+        clock.tick(30)
             
     pygame.quit()
 
